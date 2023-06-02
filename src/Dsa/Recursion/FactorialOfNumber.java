2ca6899b9e2a144b332b428ed4a2b1c960a01acf
Mayank <mayankmahajan936@gmail.com>
2023-06-02 16:38:07 +0530

    Factorial

--- src/Dsa/Recursion/FactorialOfNumber.java
+++ src/Dsa/Recursion/FactorialOfNumber.java
@@ -1,4 +1,24 @@
 package Dsa.Recursion;
 
+import java.util.Scanner;
+
 public class FactorialOfNumber {
+
+    public static int factorial(int n) {
+        int result = 0;
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            result = n * factorial(n - 1);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        int result = factorial(n);
+        System.out.println("Factorial of " + n + " is : " + result);
+    }
 }
